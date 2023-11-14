00cc695e453aa7d46298c968547677e87ae7729a
DarksoulsVII <94222096+DarksoulsVII@users.noreply.github.com>
2023-11-14 20:46:36 +0800

    Initial commit

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">name: {{ name }}</div>
+    <!--设置<icon-class></icon-class>-->
+    <svg-icon icon-class="dashboard" />
+    <svg-icon icon-class="example" />
+    <svg-icon icon-class="password" />
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
